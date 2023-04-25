79c997c2c96bc7f776e1d35d7a32ab7661c7fc8f
britfitzm <brittanyfitzm99@gmail.com>
2023-04-25 13:44:24 -0700

    bb

---
+++ simple_shell_prac/task_3.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * main - function prints a prompt for a command
+ * prompt will be displayed again after command execution
+ * @argc: argument count
+ * @argv: argument variable
+ * Return: pid
+ */
+
+int main(int argc, char *argv[])
+{
+        char *prompt = "$ ";
+        char *str;
+        size_t n = 0;
+        int var; /* this holds the getline function */
+
+        do {
+                printf("%s", prompt);
+                var = getline(&str, &n, stdin);
+
+                if (var == -1)
+                {
+                        break;
+                }
+        } while (1);
+
+        pid_t pid;
+
+	strtok("PATH");
+
+        char *argv[] = {"/usr/bin", "-l", NULL};
+
+        if (pid == -1)
+                return (-1);
+
+        if (pid == 0)
+        {
+                int val = execve(filename, argv, NULL);
+
+                if (val == -1)
+                        perror("Not Command\n");
+        }
+        else
+        {
+                printf("\n");
+        }
+
+        return (0);
+}
